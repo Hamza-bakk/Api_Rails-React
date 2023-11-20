1028829029c447865cf25545d47dda752c88e5f2
Hamza <bakkar.hamza92@gmail.com>
2023-11-20 15:27:41 +0100

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+
+require 'faker'
+
+# Supprime tous les articles existants
+Article.delete_all
+
+# Création de 30 articles avec Faker
+30.times do
+  Article.create(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraphs.join("\n\n")
+  )
+end
+
+puts "Seed finished"
