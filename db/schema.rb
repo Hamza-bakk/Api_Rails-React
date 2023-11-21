31320e38beb15dc6db501157c579ceaf8a055b29
Hamza <bakkar.hamza92@gmail.com>
2023-11-21 15:21:59 +0100

    add comment to database

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_21_104919) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
